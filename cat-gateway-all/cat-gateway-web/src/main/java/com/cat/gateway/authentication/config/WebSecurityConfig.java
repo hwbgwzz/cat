5f5152ad089c3958ca4d01f5a897ba083fcefbc2
hwb <1211525340@qq.com>
2022-07-22 13:53:56 +0800

    update pom

--- cat-gateway-all/cat-gateway-web/src/main/java/com/cat/gateway/authentication/config/WebSecurityConfig.java
+++ cat-gateway-all/cat-gateway-web/src/main/java/com/cat/gateway/authentication/config/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
